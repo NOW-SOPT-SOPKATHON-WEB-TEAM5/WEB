7a8d7ecbc247af696d52fd4c1dd1f51de4fd922d
imddoy <imddoy@duksung.ac.kr>
2024-05-17 01:46:28 +0900

    init: globalstyle 세팅

--- sopkathon/src/App.jsx
+++ sopkathon/src/App.jsx
@@ -1,8 +1,10 @@
 import Router from "./Router";
+import GlobalStyle from "./styles/Globalstyles";
 
 const App = () => {
   return (
     <>
+      <GlobalStyle />
       <Router />
     </>
   );
---
+++ sopkathon/src/styles/Globalstyles.jsx
@@ -0,0 +1,55 @@
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+const GlobalStyle = createGlobalStyle`
+
+    ${reset}
+    
+    * {
+    box-sizing: border-box;
+    }
+
+    html {
+    -webkit-touch-callout: none;
+    -webkit-tap-highlight-color:rgb(0 0 0 / 0%);
+    scroll-behavior: smooth;
+
+    font-size: 62.5%;
+    user-select: none;
+    }
+
+    ul, li {
+    padding-left: 0;
+    list-style: none;
+    }
+
+    a {
+    color: inherit;
+
+    text-decoration: none;
+    }
+
+    input, button {
+    outline: none;
+
+    border: none;
+    background-color: transparent;
+    }
+
+    button {
+    cursor: pointer;
+
+    padding: 0;
+    }
+
+    input {
+    appearance: none;
+
+    &:focus {
+        outline: none;
+    }
+    }
+
+`;
+
+export default GlobalStyle;
