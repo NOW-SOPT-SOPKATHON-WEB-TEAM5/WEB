b326938edcc9dff36f0d58569ab6a9b8bb3ff5f8
namdaeun <nde40345@gmail.com>
2024-05-19 03:18:20 +0900

    style: progress 컴포넌트 스타일링

--- sopkathon/src/components/QuestionCard/ProgressState.jsx
+++ sopkathon/src/components/QuestionCard/ProgressState.jsx
@@ -1,5 +1,17 @@
+import styled from 'styled-components';
+
 const ProgressState = ({ progress }) => {
-  return <div>{progress}</div>;
+  return <ProgressWrapper>{progress}</ProgressWrapper>;
 };
 
 export default ProgressState;
+
+const ProgressWrapper = styled.section`
+  display: flex;
+  width: 8.4rem;
+  height: 3.6rem;
+  align-items: center;
+  justify-content: center;
+  border-radius: 1.2rem;
+  background-color: ${({ theme }) => theme.colors.yellow02};
+`;
--- sopkathon/src/pages/QuestionCard.jsx
+++ sopkathon/src/pages/QuestionCard.jsx
@@ -7,7 +7,9 @@ const QuestionCard = () => {
   const [progress, setProgress] = useState(0);
   return (
     <QuestionCardWrapper>
-      <ProgressState progress={progress} />
+      <ProgressLayout>
+        <ProgressState progress={progress} />
+      </ProgressLayout>
       <Question setProgress={setProgress} />
     </QuestionCardWrapper>
   );
@@ -17,4 +19,11 @@ export default QuestionCard;
 
 const QuestionCardWrapper = styled.div`
   padding: 5.7rem 0rem 2rem 0rem;
+`;
+
+const ProgressLayout = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding-bottom: 1.8rem;
 `;
