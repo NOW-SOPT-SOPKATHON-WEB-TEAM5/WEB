f68f706bfd53c45c8ae73c7c598a14eb30eaf7d5
namdaeun <nde40345@gmail.com>
2024-05-18 23:57:26 +0900

    feat: 라우터 element 추가

--- sopkathon/src/Router.jsx
+++ sopkathon/src/Router.jsx
@@ -1,11 +1,13 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Main from "./pages/Main";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Main from './pages/Main';
+import QuestionCard from './pages/QuestionCard';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Main />} />
+        <Route path="/question" element={<QuestionCard />} />
       </Routes>
     </BrowserRouter>
   );
