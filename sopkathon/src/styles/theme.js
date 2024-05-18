195377b8239f9087e62d7c97d9274c72efc6b980
namdaeun <nde40345@gmail.com>
2024-05-18 21:46:19 +0900

    Feat: Theme 적용

---
+++ sopkathon/src/styles/theme.js
@@ -0,0 +1,76 @@
+const colors = {
+  black: '#1E1E1E',
+  white: '#FFFFFF',
+  gray900: '#1B1D1F',
+  gray800: '#26282B',
+  gray700: '#464C52',
+  gray600: '#73787E',
+  gray500: '#9FA4A9',
+  gray400: '#CACDD2',
+  gray300: '#E9EBED',
+  gray100: '#F7F8F9',
+};
+
+const fonts = {
+  Heading1: {
+    fontFamily: "'Pretendard Variable', sans-serif",
+    fontWeight: 700,
+    fontSize: '2.4rem',
+    lineHeight: '3.12rem',
+  },
+  Heading2: {
+    fontFamily: "'Pretendard Variable', sans-serif",
+    fontWeight: 700,
+    fontSize: '2.1rem',
+    lineHeight: '2.73rem',
+  },
+  Heading3: {
+    fontFamily: "'Pretendard Variable', sans-serif",
+    fontWeight: 600,
+    fontSize: '1.8rem',
+    lineHeight: '2.34rem',
+  },
+  Body1: {
+    fontFamily: "'Pretendard Variable', sans-serif",
+    fontWeight: 600,
+    fontSize: '1.6rem',
+    lineHeight: '2.08rem',
+  },
+  Body2: {
+    fontFamily: "'Pretendard Variable', sans-serif",
+    fontWeight: 500,
+    fontSize: '1.6rem',
+    lineHeight: '2.08rem',
+  },
+  Body3: {
+    fontFamily: "'Pretendard Variable', sans-serif",
+    fontWeight: 500,
+    fontSize: '1.4rem',
+    lineHeight: '1.82rem',
+  },
+  Body4: {
+    fontFamily: "'Pretendard Variable', sans-serif",
+    fontWeight: 500,
+    fontSize: '1.3rem',
+    lineHeight: '1.69rem',
+  },
+  Body5: {
+    fontFamily: "'Pretendard Variable', sans-serif",
+    fontWeight: 500,
+    fontSize: '1.2rem',
+    lineHeight: '1.56rem',
+  },
+  Detail1: {
+    fontFamily: "'Pretendard Variable', sans-serif",
+    fontWeight: 500,
+    fontSize: '1.0rem',
+    lineHeight: '1.3rem',
+  },
+};
+
+const theme = {
+  colors,
+  fonts,
+};
+
+export default theme;
