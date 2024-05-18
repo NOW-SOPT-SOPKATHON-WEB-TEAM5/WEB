c713e9d49a629eb0b8492569e0de85712a68a971
namdaeun <nde40345@gmail.com>
2024-05-19 03:59:55 +0900

    feat: progress에 맞게 icon 렌더링

--- sopkathon/src/components/QuestionCard/ProgressState.jsx
+++ sopkathon/src/components/QuestionCard/ProgressState.jsx
@@ -1,7 +1,21 @@
 import styled from 'styled-components';
+import IcChecked from '../../assets/IcProgressChecked.svg?react';
+import IcNotchecked from '../../assets/IcProgressNotChecked.svg?react';
 
 const ProgressState = ({ progress }) => {
-  return <ProgressWrapper>{progress}</ProgressWrapper>;
+  const progressArray = Array.from({ length: progress });
+
+  return (
+    <ProgressWrapper>
+      {progressArray.map((_, index) => (
+        <IconChecked key={index} />
+      ))}
+      {progressArray.length < 3 &&
+        Array.from({ length: 3 - progressArray.length }).map((_, index) => (
+          <IconNotchecked key={index} />
+        ))}
+    </ProgressWrapper>
+  );
 };
 
 export default ProgressState;
@@ -13,5 +27,16 @@ const ProgressWrapper = styled.section`
   align-items: center;
   justify-content: center;
   border-radius: 1.2rem;
+  gap: 0.4rem;
   background-color: ${({ theme }) => theme.colors.yellow02};
+`;
+
+const IconChecked = styled(IcChecked)`
+  width: 2rem;
+  height: 2rem;
+`;
+
+const IconNotchecked = styled(IcNotchecked)`
+  width: 2rem;
+  height: 2rem;
 `;
